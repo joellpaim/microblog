ae94b2908a49e1462915ee49faa694f0048af3b1
Ian Dolge <dolge.erau@gmail.com>
2022-07-31 13:58:32 -0700

    Chapter 3: Web Forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
+
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -11,3 +13,15 @@ def index():
         {"author": {"username": "Susan"}, "body": "The avengers move was so cool!"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
+
+    return render_template("login.html", title="Sign In", form=form)
