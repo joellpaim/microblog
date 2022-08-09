1d67cc1b1a9d259073a078b6e574ace37e0a207f
Ian Dolge <dolge.erau@gmail.com>
2022-08-08 19:53:48 -0700

    Chapter 10: Email Support

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from hashlib import md5
+from time import time
 
+import jwt
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app import db, login
+from app import app, db, login
 
 followers = db.Table(
     "followers",
@@ -59,6 +61,23 @@ class User(UserMixin, db.Model):
             followers, (followers.c.followed_id == Post.user_id)
         ).filter(followers.c.follower_id == self.id)
         return followed.union(self.posts).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {"reset_password": self.id, "exp": time() + expires_in},
+            app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])[
+                "reset_password"
+            ]
+        except:
+            return
+        return User.query.get(id)
 
 
 @login.user_loader
